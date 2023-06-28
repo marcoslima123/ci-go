ce197459cc926c587c7c63d066d462df84b11d82
Marcos Pereira <marcoslima_1212@hotmail.com>
2023-06-28 11:53:12 -0300

    ci: ignore dockerfile

--- math.go
+++ math.go
@@ -9,15 +9,3 @@ func main() {
 func Soma(a int, b int) int {
 	return a + b
 }
-
-func Subtracao(a int, b int) int {
-	return a - b
-}
-
-func Times(a int, b int) int {
-	return a * b
-}
-
-func Divisao(a int, b int) int {
-	return a / b
-}
